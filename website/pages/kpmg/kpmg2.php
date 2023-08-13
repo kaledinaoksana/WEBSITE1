d96db2392c7f712893b65a36b1a3e03d22d9c212
‘kaledinaoa’ <kaledinaoa@gmail.com>
2023-08-14 00:38:37 +0400

    add kpmg blocks

---
+++ website/pages/kpmg/kpmg2.php
@@ -0,0 +1,18 @@
+
+<h3>II. Data Insights</h3>
+
+<p><b>Links</b></p>
+
+<a href="https://www.abs.gov.au/statistics?opendocument&ref=topBar" ><p class="label"  >Australian Bureau of Statistics</p></a></li>
+
+<p><b>Corellation</b></p>
+<p> <code>age</code> and <code>tenture</code> = 0.45</p>
+<p> <code>list_price</code> and <code>standard_cost</code> = 0.55</p>
+
+
+<a style="padding: 0% 0% 0% 0%;">
+    <img src="../../../website/images/corr.png" class="img-fluid" alt="" />
+</a>
+
+
+<!-- <a class="image main"><img src="images/corr.png" alt="" /></a> -->
