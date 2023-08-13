5d33daceb9fdfd6568765dae38edf0eed094b678
‘kaledinaoa’ <kaledinaoa@gmail.com>
2023-08-14 00:40:24 +0400

    add cv blocks

---
+++ website/blocks/head.php
@@ -0,0 +1,18 @@
+<!-- Meta -->
+<meta charset="utf-8">
+<link rel="shortcut icon" href="../images/favicon.ico"> 
+
+<!-- Google Fonts -->	
+<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!--
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/main.css" />
+-->
+<!-- Bootstrpa Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<!-- FontAwesome JS -->
+<script defer src="../assets/fontawesome/js/all.js"></script>
+
+<!-- Theme CSS -->  
+<link id="theme-style" rel="stylesheet" href="../assets/css/risen.css">
