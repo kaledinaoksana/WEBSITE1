5e09b647b8d90472cf7ce3bac9f0f52601c28a58
‘kaledinaoa’ <kaledinaoa@gmail.com>
2023-08-14 00:39:21 +0400

    add cv blocks

---
+++ website/cv_blocks/download_cv.php
@@ -0,0 +1,4 @@
+<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+<a href="/website/docs/KaledinaOA_CV.pdf" download style="color: white">
+<i class="resume-section-heading-icon bi bi-file-earmark-pdf"></i> Download CV</a></h3>
+<hr>
---
+++ website/cv_blocks/work_analyst.php
@@ -0,0 +1,40 @@
+<div class="resume-timeline-item-header mb-2">
+												    
+    <div class="resume-position-meta d-flex justify-content-between mb-1">
+        <div class="resume-position-time">Jul 2022 - Present</div>
+        <div class="resume-company-name">Self-employment</div>
+    </div>
+    <h3 class="resume-position-title mb-1">Data Analyst</h3>
+
+</div><!--//resume-timeline-item-header-->
+<div class="resume-timeline-item-desc">
+    
+    
+    <ol class="resume-timeline-list" style="text-align: justify">
+        
+        <li>KPMG Data Analytics Consulting Virtual Internship</li>
+            <ul>
+                <li>Assessment of data quality and completeness in preparation for analysis (Excel)</li>
+                <li>Targeting high value customers based on customer demographics and attributes (A/B Testing)</li>
+                <li>Using visualizations to present insights (PowerBI)</li>
+            </ul>
+
+        <li>Data Analyst Bootcamp</li>
+            <ul>
+                <li>Analyzed COVID-19 data in Russia and USA</li>
+                <li>Using Docker, Azure Data Studio, SQL, SSMS, PowerBI, Excel</li>
+            </ul>
+        <li > <a href="https://github.com/kaledinaoksana/website" style="color: white"> Portfolio Website</a>
+        
+        </li>
+            <ul>
+                <li>My portfolio is a website developed using Bootstrap. It showcases my web 
+                    development skills and creativity. It features a diverse 
+                    range of projects, each accompanied by descriptions and code samples on GitHub. 
+                    Additionally, it includes a contact form and an "About Me" section with information 
+                    about myself and my skills.</li>
+            </ul>
+        
+    </ol>
+    
+</div><!--//resume-timeline-item-desc-->
